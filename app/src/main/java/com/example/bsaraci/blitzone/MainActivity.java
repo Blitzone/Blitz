b8fe7aea160f76f00ea12cf0332c096e37ff1783
brajansaraci <brajansaraci@gmail.com>
2016-02-14 12:54:14 -1200

    Some tests just to get used to github

--- app/src/main/java/com/example/bsaraci/blitzone/MainActivity.java
+++ app/src/main/java/com/example/bsaraci/blitzone/MainActivity.java
@@ -3,11 +3,18 @@ package com.example.bsaraci.blitzone;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+    public class MainActivity extends AppCompatActivity
+    {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    private void testUpload()
+    {
+
+    }
 }
+
---
+++ app/src/main/java/com/example/bsaraci/blitzone/Test.java
@@ -0,0 +1,4 @@
+package com.example.bsaraci.blitzone;
+
+public class Test {
+}
