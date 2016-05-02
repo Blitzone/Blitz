bbef74ec3d9fb07ec5ed5804fdd773e56c958474
bsaraci <brajansaraci@outlook.com>
2016-05-02 11:27:03 +0200

    fixed profile photo

--- app/src/main/java/com/example/bsaraci/blitzone/Profile/Profile.java
+++ app/src/main/java/com/example/bsaraci/blitzone/Profile/Profile.java
@@ -7,6 +7,7 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
@@ -25,6 +26,10 @@ import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.load.resource.drawable.GlideDrawable;
+import com.bumptech.glide.request.animation.GlideAnimation;
+import com.bumptech.glide.request.target.GlideDrawableImageViewTarget;
 import com.example.bsaraci.blitzone.Blitzone.Blitzone;
 import com.example.bsaraci.blitzone.Start.LogIn;
 import com.example.bsaraci.blitzone.Notifications.Notifications;
@@ -127,11 +132,11 @@ public class Profile extends AppCompatActivity {
             } else if (requestGallery == UPLOAD_CHAPTER_IMAGE_FROM_GALLERY) {
                 Chapter chap = profilePhotosDataSet.getChapter(chapterClicked);
                 profilePhotosDataSet.addPhotoChapter(photoChapter, chap);
+                dialog = ProgressDialog.show(Profile.this, "", "Uploading chapter image ...", true);
+                uploadPicture(photoChapter.getBitmap(), RequestURL.UPLOAD_USER_CHAPTER, getPhotoChapterParams(chap));
                 mAdapter = new ProfileRecyclerviewAdapter(profilePhotosDataSet,this);
                 mAdapter.notifyDataSetChanged();
                 mRecyclerView.setAdapter(mAdapter);
-                dialog = ProgressDialog.show(Profile.this, "", "Uploading chapter image ...", true);
-                uploadPicture(photoChapter.getBitmap(), RequestURL.UPLOAD_USER_CHAPTER, getPhotoChapterParams(chap));
             }
         }
     }
@@ -317,11 +322,28 @@ public class Profile extends AppCompatActivity {
             String avatar = RequestURL.IP_ADDRESS + response.get("avatar").toString();
 
             final ImageView imageView = (ImageView) this.findViewById(R.id.profile_picture);
+            final ImageView imageView1 = (ImageView) this.findViewById(R.id.profile_picture1);
 
             Glide.with(this)
                     .load(avatar)
-                    .crossFade()
-                    .into(imageView);
+                    .diskCacheStrategy(DiskCacheStrategy.SOURCE)
+                    .placeholder(R.color.white)
+                    .dontAnimate()
+                    .into(new GlideDrawableImageViewTarget(imageView) {
+                        @Override
+                        public void onResourceReady(GlideDrawable resource, GlideAnimation<? super GlideDrawable> animation) {
+                            super.onResourceReady(resource, animation);
+                            imageView1.setVisibility(View.GONE);
+                            //never called
+                        }
+
+                        @Override
+                        public void onLoadFailed(Exception e, Drawable errorDrawable) {
+                            super.onLoadFailed(e, errorDrawable);
+                            Toast.makeText(Profile.this, "Error loading profile picture", Toast.LENGTH_SHORT).show();
+                            //never called
+                        }
+                    });
 
             TextView blitzCountView = (TextView) findViewById(R.id.number_of_blitz);
             blitzCountView.setText(blitzCount.toString());
--- app/src/main/java/com/example/bsaraci/blitzone/Profile/ProfileRecyclerviewAdapter.java
+++ app/src/main/java/com/example/bsaraci/blitzone/Profile/ProfileRecyclerviewAdapter.java
@@ -12,6 +12,7 @@ import android.widget.TextView;
 
 import com.android.volley.toolbox.ImageLoader;
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.bumptech.glide.load.resource.drawable.GlideDrawable;
 import com.bumptech.glide.request.RequestListener;
 import com.bumptech.glide.request.target.Target;
@@ -69,6 +70,7 @@ public class ProfileRecyclerviewAdapter extends RecyclerView.Adapter<ProfileRecy
             holder.progressBar.setVisibility(View.VISIBLE);
             Glide.with(this.context)
                     .load(photoChapter1.getUrl())
+                    .diskCacheStrategy(DiskCacheStrategy.SOURCE)
                     .listener(new RequestListener<String, GlideDrawable>() {
                         @Override
                         public boolean onException(Exception e, String model, Target<GlideDrawable> target, boolean isFirstResource) {
