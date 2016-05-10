2c4aecfc202564feb891e31f77babbae8614ce50
bsaraci <brajansaraci@outlook.com>
2016-05-10 11:37:42 +0200

    search updated again

--- app/src/main/java/com/example/bsaraci/blitzone/Search/Search.java
+++ app/src/main/java/com/example/bsaraci/blitzone/Search/Search.java
@@ -1,6 +1,8 @@
 package com.example.bsaraci.blitzone.Search;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v4.app.FragmentTransaction;
@@ -16,10 +18,23 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.example.bsaraci.blitzone.Profile.Topic;
 import com.example.bsaraci.blitzone.R;
+import com.example.bsaraci.blitzone.ServerComm.JWTManager;
+import com.example.bsaraci.blitzone.ServerComm.MRequest;
+import com.example.bsaraci.blitzone.ServerComm.RequestQueueSingleton;
+import com.example.bsaraci.blitzone.ServerComm.RequestURL;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 
@@ -30,6 +45,8 @@ public class Search extends AppCompatActivity
     SearchView sv;
     RecyclerView rv;
     TextView chapter1,chapter2,chapter3,chapter4,chapter5;
+    Topic topic;
+    RelativeLayout chapterContainer;
 
 
     @Override
@@ -38,11 +55,8 @@ public class Search extends AppCompatActivity
         setContentView(R.layout.search_main);
         searchToolbar = (Toolbar) findViewById(R.id.toolbar_of_search);
         toolbarTitle = (TextView) findViewById(R.id.search_toolbar_title);
-        chapter1 = (TextView)findViewById(R.id.chapter1);
-        chapter2 = (TextView)findViewById(R.id.chapter2);
-        chapter3 = (TextView)findViewById(R.id.chapter3);
-        chapter4 = (TextView)findViewById(R.id.chapter4);
-        chapter5 = (TextView)findViewById(R.id.chapter5);
+        chapterContainer = (RelativeLayout) findViewById(R.id.chapterContainer);
+        getTopic();
 
         rv= (RecyclerView) findViewById(R.id.myRecycler);
         sv= (SearchView) findViewById(R.id.mSearch);
@@ -52,13 +66,7 @@ public class Search extends AppCompatActivity
             public void onClick(View v) {
                 toolbarTitle.setVisibility(View.GONE);
                 rv.setVisibility(View.VISIBLE);
-                chapter1.setVisibility(View.GONE);
-                chapter2.setVisibility(View.GONE);
-                chapter3.setVisibility(View.GONE);
-                chapter4.setVisibility(View.GONE);
-                chapter5.setVisibility(View.GONE);
-
-
+                visibleLayout(chapterContainer);
             }
         });
         sv.setOnCloseListener(new SearchView.OnCloseListener() {
@@ -66,11 +74,7 @@ public class Search extends AppCompatActivity
             public boolean onClose() {
                 toolbarTitle.setVisibility(View.VISIBLE);
                 rv.setVisibility(View.GONE);
-                chapter1.setVisibility(View.VISIBLE);
-                chapter2.setVisibility(View.VISIBLE);
-                chapter3.setVisibility(View.VISIBLE);
-                chapter4.setVisibility(View.VISIBLE);
-                chapter5.setVisibility(View.VISIBLE);
+                invisibleLayout(chapterContainer);
                 return false;
             }
         });
@@ -83,12 +87,6 @@ public class Search extends AppCompatActivity
         rv.setAdapter(adapter);
 
         //SEARCH
-        sv.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(Search.this,"text",Toast.LENGTH_LONG).show();
-            }
-        });
         sv.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String query) {
@@ -104,8 +102,39 @@ public class Search extends AppCompatActivity
             }
         });
 
+    }
 
+    public void initiateTextViews(){
+        try{
+            chapter1 = (TextView)findViewById(R.id.chapter1);
+            chapter1.setText(topic.getPhotoChapterFromPosition(0).getChapterName());
+            chapterCallback(chapter1);
+            chapter2 = (TextView)findViewById(R.id.chapter2);
+            chapter2.setText(topic.getPhotoChapterFromPosition(1).getChapterName());
+            chapterCallback(chapter2);
+            chapter3 = (TextView)findViewById(R.id.chapter3);
+            chapter3.setText(topic.getPhotoChapterFromPosition(2).getChapterName());
+            chapterCallback(chapter3);
+            chapter4 = (TextView)findViewById(R.id.chapter4);
+            chapter4.setText(topic.getPhotoChapterFromPosition(3).getChapterName());
+            chapterCallback(chapter4);
+            chapter5 = (TextView)findViewById(R.id.chapter5);
+            chapter5.setText(topic.getPhotoChapterFromPosition(4).getChapterName());
+            chapterCallback(chapter5);
 
+        }
+        catch (IndexOutOfBoundsException e){
+
+        }
+
+    }
+
+    public void invisibleLayout(RelativeLayout relativeLayout){
+        relativeLayout.setVisibility(View.GONE);
+    }
+
+    public void visibleLayout(RelativeLayout relativeLayout){
+       relativeLayout.setVisibility(View.VISIBLE);
     }
 
     //ADD USERS TO ARRAYLIST
@@ -182,9 +211,125 @@ public class Search extends AppCompatActivity
         finish();
     }
 
-    public void chapterOneCallback (View view){
-        Intent intent = new Intent(Search.this,GridViewSearch.class);
-        intent.putExtra("toolbarTitle",chapter1.getText());
-        startActivity(intent);
+    public void chapterCallback (TextView textView){
+        if(textView==chapter1){
+            textView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(Search.this, GridViewSearch.class);
+                    intent.putExtra("toolbarTitle", chapter1.getText());
+                    startActivity(intent);
+                }
+            });
+        }
+        else if(textView==chapter2){
+            textView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(Search.this, GridViewSearch.class);
+                    intent.putExtra("toolbarTitle", chapter2.getText());
+                    startActivity(intent);
+                }
+            });
+        }
+        else if(textView==chapter3){
+            textView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(Search.this, GridViewSearch.class);
+                    intent.putExtra("toolbarTitle", chapter3.getText());
+                    startActivity(intent);
+                }
+            });
+        }
+        else if(textView==chapter4){
+            textView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(Search.this, GridViewSearch.class);
+                    intent.putExtra("toolbarTitle", chapter4.getText());
+                    startActivity(intent);
+                }
+            });
+
+        }
+        else if(textView==chapter5){
+            textView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(Search.this, GridViewSearch.class);
+                    intent.putExtra("toolbarTitle", chapter5.getText());
+                    startActivity(intent);
+                }
+            });
+
+        }
+    }
+
+    private void getTopic() {
+        Response.Listener<JSONObject> listener = new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                updateTopic(response);
+            }
+        };
+
+        //Function to be executed in case of an error
+        Response.ErrorListener errorListener = new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("Error", error.toString());
+            }
+        };
+
+        JWTManager jwtManager = new JWTManager(getApplicationContext());
+        //Put everything in the request
+        MRequest mRequest = new MRequest(
+                RequestURL.TOPIC,
+                Request.Method.GET,
+                null, //Put the parameters of the request here (JSONObject format)
+                listener,
+                errorListener,
+                jwtManager
+        );
+
+        RequestQueueSingleton.getInstance(this).addToRequestQueue(mRequest);
+    }
+
+    private void updateTopic(JSONObject response) {
+        try {
+            JSONObject jsonTopic = (JSONObject)response.get("topic");
+            JSONArray jsonChapterList = (JSONArray)response.get("chapters");
+
+            Integer topicId     = (Integer)jsonTopic.get("id");
+            String topicName    = jsonTopic.getString("name");
+            topic = new Topic(topicId, topicName);
+
+
+            updateChapters(jsonChapterList);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private void updateChapters(JSONArray chapterList) {
+
+        try {
+            int chapterListSize = chapterList.length();
+            Bitmap bitmap = BitmapFactory.decodeResource(this.getResources(), R.color.lightGray);
+            for (int i = 0; i < chapterListSize; i++) {
+                JSONObject jsonChapter = (JSONObject) chapterList.getJSONObject(i);
+                Integer chapterId = (Integer) jsonChapter.get("id");
+                String chapterName = jsonChapter.get("name").toString();
+                topic.addPhotoChapter(chapterId, chapterName);
+                topic.addPhotoByChapterId(chapterId, bitmap);
+            }
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        initiateTextViews();
     }
 }
